f62e1575e6719d0ea76b28f6aa53551a07175724
Nick Joyce <brawlyrox@gmail.com>
2018-03-12 21:11:37 +0000

    added background colour change on refresh with transition

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { quote: 'This is a random quote just to get you started' };
+    this.state = { quote: 'This is a random quote just to get you started', color: 'white' };
     this.randomQuote = this.randomQuote.bind(this);
   }
   randomQuote() {
     const quotes = [
-      'This is a new quote, displayed by the quote randomiser 1.',
+      'Why are there no free range chickens, this has ruined my weekend!',
       'This is a new quote, displayed by the quote randomiser 2.',
       'This is a new quote, displayed by the quote randomiser 3.',
       'This is a new quote, displayed by the quote randomiser 4.',
       'This is a new quote, displayed by the quote randomiser 5.'
     ];
+    
+    const colors = [
+      '#39CCCC',
+      '#85144b',
+      '#FF4136',
+      '#7FDBFF',
+      '#3D9970'
+    ];
+
     var randNum = Math.floor(Math.random() * (quotes.length)) + 0;
-    this.setState({ quote: quotes[randNum] });
+    var randNum2 = Math.floor(Math.random() * (colors.length)) + 0;
+    this.setState({ quote: quotes[randNum]});
+    document.body.style.backgroundColor = colors[randNum2];
   };
   render() {
       let returnQuote  = this.state.quote;
@@ -26,16 +37,16 @@ class App extends Component {
       <div>
         <div className="card">
           <div className="card-block">
-            <h4 className="card-title">Quote Randomiser</h4>
+            <h3 className="card-title">Megulator</h3>
             <p className="card-text">{this.state.quote}</p>
-            <h6 className="card-subtitle mb-2 text-muted">- Author</h6>
+            <h6 className="card-subtitle mb-2 text-muted">-Megan Phillips</h6>
             <div className="row">
               <div className="action col-sm">
                 <button onClick={this.randomQuote} className="btn btn-primary" >New Quote</button>
               </div>
               <div className="col-sm">
                 <div className="tweet">
-                <a class="button"  title="Tweet this quote!" target="_blank" href={"https://twitter.com/intent/tweet?text=" + returnQuote}>
+                <a class="button"  title="Tweet this quote!" target="_blank" href={"https://twitter.com/intent/tweet?text=" + returnQuote + ' -Megan Phillips'}>
                 <i className="fab fa-twitter-square"></i></a>
                 </div>
               </div>

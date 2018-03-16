ee74f8a6fd18f1cfc903f3f4d0704113a0fa4e97
Nick Joyce <brawlyrox@gmail.com>
2018-03-16 18:09:08 +0000

    Changed to now run from the API, removed the hardcoded quotes. Add in the author variable

--- src/App.js
+++ src/App.js
@@ -1,52 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { quote: 'This is a random quote just to get you started', color: 'white' };
+    this.state = { 
+    error: null,
+    isLoaded: false,
+    items: []
+  };
     this.randomQuote = this.randomQuote.bind(this);
   }
+  
   randomQuote() {
-    const quotes = [
-      'Why are there no free range chickens, this has ruined my weekend!',
-      'This is a new quote, displayed by the quote randomiser 2.',
-      'This is a new quote, displayed by the quote randomiser 3.',
-      'This is a new quote, displayed by the quote randomiser 4.',
-      'This is a new quote, displayed by the quote randomiser 5.'
-    ];
+
+    fetch("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous", {
+      headers: { 'X-Mashape-Key': '95xnlXQbHHmshx9yQ6nMBY5Z0XI8p1tmlDIjsn1Mk9E8yRwZCM'},
+  })
+  .then(res => res.json())
+  .then(
+    (result) => {
+      this.setState({
+        isLoaded: true,
+        items: result
+      });
+    },
+    // Note: it's important to handle errors here
+    // instead of a catch() block so that we don't swallow
+    // exceptions from actual bugs in components.
+    (error) => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    }
+  );
     
     const colors = [
       '#39CCCC',
       '#85144b',
       '#FF4136',
       '#7FDBFF',
-      '#3D9970'
+      '#3D9970',
+      '#FFDC00',
+      '#01FF70',
+      '#2ECC40',
+      '#FF851B'
     ];
-
-    var randNum = Math.floor(Math.random() * (quotes.length)) + 0;
+    
     var randNum2 = Math.floor(Math.random() * (colors.length)) + 0;
-    this.setState({ quote: quotes[randNum]});
     document.body.style.backgroundColor = colors[randNum2];
+  
   };
+
+componentDidMount() {
+  this.randomQuote();
+}
+
   render() {
-      let returnQuote  = this.state.quote;
+      
+
+       let returnQuote  = this.state.items.quote;
+       let returnAuthor = this.state.items.author;
+
+       
+      
+      
     return (
       
       <div>
         <div className="card">
           <div className="card-block">
-            <h3 className="card-title">Megulator</h3>
-            <p className="card-text">{this.state.quote}</p>
-            <h6 className="card-subtitle mb-2 text-muted">-Megan Phillips</h6>
+            <h3 className="card-title">Quote Generator</h3>
+            <br />      
+            <p className="card-text">{this.state.items.quote}</p>
+            <div></div>
+            <h6 className="card-subtitle mb-2 text-muted">-{this.state.items.author}</h6>  
             <div className="row">
               <div className="action col-sm">
                 <button onClick={this.randomQuote} className="btn btn-primary" >New Quote</button>
               </div>
               <div className="col-sm">
                 <div className="tweet">
-                <a class="button"  title="Tweet this quote!" target="_blank" href={"https://twitter.com/intent/tweet?text=" + returnQuote + ' -Megan Phillips'}>
+                <a className="button"  title="Tweet this quote!" target="_blank" href={"https://twitter.com/intent/tweet?hashtags=quotes,famous&text=" + returnQuote + ' -' + returnAuthor}>
                 <i className="fab fa-twitter-square"></i></a>
                 </div>
               </div>

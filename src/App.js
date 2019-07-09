9e62e85dee77072bc98902f51fe025272c95d346
Nick Joyce <brawlyrox@gmail.com>
2019-07-09 17:40:41 +0100

    Added state with initial data array

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ class App extends Component {
     this.state = { 
     error: null,
     isLoaded: false,
-    items: []
+    items: [],
+    data: [{
+      author: 'Loading...',
+      quote: 'Loading...'
+    }]
   };
     this.randomQuote = this.randomQuote.bind(this);
   }
@@ -25,28 +29,6 @@ class App extends Component {
   this.setState({data: json})
   console.log(this.state.data)
   console.log(this.state.data[0])
-  //.then(res => res.json())
-//   .then(
-//     (result) => {
-//       this.setState({
-//         isLoaded: true,
-//         items: result
-//  });
-
-//     },
-//     // Note: it's important to handle errors here
-//     // instead of a catch() block so that we don't swallow
-//     // exceptions from actual bugs in components.
-//     (error) => {
-//       this.setState({
-//         isLoaded: true,
-//         error
-//       });
-//     }
-//   );
-    
-this.author = this.state.data[0].author
-this.quote = this.state.data[0].quote
 
     const colors = [
       '#39CCCC',
@@ -65,28 +47,28 @@ this.quote = this.state.data[0].quote
   
   };
 
+  async componentWillMount() {
+    const result = await fetch("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous", {
+      headers: { 'X-Mashape-Key': '95xnlXQbHHmshx9yQ6nMBY5Z0XI8p1tmlDIjsn1Mk9E8yRwZCM' },
+    })
 
 
-
-  async componentDidMount() {
-   await this.randomQuote();
-
-
+    const json = await result.json()
+    this.setState({ data: json })
 }
 
   render() {
-      
 
     return (
-      
+         
       <div>
         <div className="card">
           <div className="card-block">
             <h3 className="card-title">Quote Generator</h3>
             <br />      
-            <p className="card-text">{this.quote}</p>
+            <p className="card-text">{this.state.data[0].quote || 'Loading'}</p>
             <div></div>
-            <h6 className="card-subtitle mb-2 text-muted">-{this.author}</h6>  
+            <h6 className="card-subtitle mb-2 text-muted">-{this.state.data[0].author || 'Loading'}</h6>  
             <div className="row">
               <div className="action col-sm">
                 <button onClick={this.randomQuote} className="btn btn-primary" >New Quote</button>
